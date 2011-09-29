7651016ae540b71ec48ff19e02765725554d47e6
mattdennewitz <mattdennewitz@gmail.com>
2011-09-28 21:17:36 -0500

    bumped to 0.2.3

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ f.close()
 
 setup(
     name='django-undermythumb',
-    version='0.2.2',
+    version='0.2.3',
     description="""
 The Django thumbnail generation package with a heart of stone.
     """.strip(),
